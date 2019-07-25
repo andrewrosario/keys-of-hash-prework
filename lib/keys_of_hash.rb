3e83033149fdcf73c0946a48ab68185aa349f02c
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-25 16:02:48 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,6 +4,10 @@ class Hash
     array_of_keys = []
     self.each do |key, value|
       puts "Key is #{key}. Value is #{value}"
+      if value == arguments
+        array_of_keys << key
+      end
     end
+    array_of_keys
   end
 end
