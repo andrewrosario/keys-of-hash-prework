eb69ba938ea1fe399958c0a69a9ebbb755df394b
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-25 15:50:44 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,9 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
     array_of_keys = []
-    
+    arguments.each do |key, value|
+      puts key
+    end
   end
 end
